5f4c82b85b4eee6136fddfca3180176192f11d1c
Luis Bucio <luis.mtz.bucio@gmail.com>
2021-07-27 15:09:30 -0500

    Funcion de carrito

--- assets/js/carrito.js
+++ assets/js/carrito.js
@@ -1,10 +1,5 @@
-const addArticle = document.querySelectorAll('.comprar');
-addArticle.forEach(addToCardButton =>{
-    addToCardButton.addEventListener('click',addToCardClicked);
-})
-
-function addToCardClicked(event){
-const boton =event.target;
-const item = button.closest('.clean-product-item');
-console.log(item.value);
+var a = 0;
+function myFunction() {
+  a = a + 1;
+  document.getElementById("demo").textContent = a;
 }
