b6f416aad85bd402cc118f888152477f8610931a
Astrik Torres <astriktorres@gmail.com>
2021-08-05 11:21:00 -0500

    LogIn configurado

---
+++ assets/js/login.js
@@ -0,0 +1,46 @@
+const formLogin = document.querySelector("#formLogin");
+
+formLogin.addEventListener("submit", (e) => {
+
+    e.preventDefault();
+    const email = document.querySelector("#email");
+    const password = document.querySelector("#password");
+
+    if(email.value == '') {
+        alert("ingresa un email")
+    }
+
+    if(password.value == '') {
+        alert("ingresa una contraseña")
+    }
+
+    if (email.value != '' && password.value != '') {
+    
+        fetch('http://localhost:8080/login', {
+            method: "POST",
+            body: JSON.stringify({
+                username: email.value,
+                password: password.value
+            }),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }).then(resp => {
+            token = resp.headers.get("Authorization");
+
+            if (token && token.includes('Bearer')) {
+                console.log(token);
+                localStorage.setItem("token", token);
+                /* window.location.href = "succes.html"; */
+                url = window.location;
+                const path = url.pathname.substring(0, url.pathname.lastIndexOf('/') + 1);
+
+                location.href = path + "index.html";
+            } else {
+                localStorage.removeItem("token");            
+                alert("Error, favor de ingresar un email y un password validos");
+            }
+            
+        })
+    }
+})
