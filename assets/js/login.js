78eaa0cdbce448d63ec81ecc89ef2d15c19a7409
Astrik Torres <astriktorres@gmail.com>
2021-08-06 13:26:57 -0500

    Comprobacion de token para que no puedas iniciar sesion de nuevo

--- assets/js/login.js
+++ assets/js/login.js
@@ -44,3 +44,12 @@ formLogin.addEventListener("submit", (e) => {
         })
     }
 })
+
+token = localStorage.getItem('token');
+
+if (token) {
+    url = window.location;
+    const path = url.pathname.substring(0, url.pathname.lastIndexOf('/') + 1);
+    location.href = path + "perfil.html";
+
+}
